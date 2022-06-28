7c9bbd2bb106ac0944e22096685b7b512e80c470
Amark19 <amarkhamkar6@gmail.com>
2022-06-28 13:16:42 +0530

    worldtime api fetch

--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class ChooseLoc extends StatefulWidget {
   _chooseLocState createState() => _chooseLocState();
 }
 
 class _chooseLocState extends State<ChooseLoc> {
-  void getData() async {
-    String username = await Future.delayed(
-      Duration(seconds: 3),
-      () => "amar",
-    );
-    String bio = await Future.delayed(
-      Duration(seconds: 3),
-      () => "I am 21 yrs old,doing good!",
-    );
-    print('$username - $bio');
+  void getTime() async {
+    var url = Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Kolkata');
+    var response = await http.get(url);
+    Map data = convert.jsonDecode(response.body);
+
+    //get properties of datetime
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    print(offset);
+
+    //creating datetime object
+    DateTime now = DateTime.parse(datetime);
+    // now = now.add(Duration(hours: int.parse(offset)));
+    print(datetime);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
-    print("hello");
+    getTime();
   }
 
   @override
